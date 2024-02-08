471aa7111187541d110ea902b4c2c265376528f6
Betulcan7 <9019870@student.zadkine.nl>
2024-02-08 13:58:02 +0100

    oop

--- huizen.php
+++ huizen.php
@@ -1,6 +1,8 @@
 <?php
 
 class House {
+
+    // attributen van het huis
     private $floor;
     private $rooms;
     private $width;
@@ -8,15 +10,18 @@ class House {
     private $depth;
     private $volume;
 
+    // constructor 
     public function __construct($floor, $rooms, $width, $height, $depth) {
         $this->floor = $floor;
         $this->rooms = $rooms;
         $this->width = $width;
         $this->height = $height;
         $this->depth = $depth;
+        // berekent het volume op basis van de breedte, hoogte en diepte van het huis
         $this->setVolume($width, $height, $depth);
     }
 
+    // methode om het volume van het huis in te stellen
     private function setVolume($width, $height, $depth) {
         $this->volume = $width * $height * $depth;
     }
@@ -25,8 +30,9 @@ class House {
         return "Dit huis heeft {$this->floor} verdiepingen, {$this->rooms} kamers en heeft een volume van {$this->volume}m3.";
     }
 
+    // methode om de prijs van het huis te berekenen
     public function getPrice() {
-        $price_per_cubic_meter = 1500; // Price per cubic meter
+        $price_per_cubic_meter = 1500; // prijs per kubieke meter
         $price = $this->volume * $price_per_cubic_meter;
         return "De prijs van het huis is: " . number_format($price, 0, ",", ".");
     }
